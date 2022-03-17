c83b5b33ea3e02ee7e4102883c251ec05149bceb
Monika <monikawozniak07@gmail.com>
2022-03-17 20:05:26 +0100

    fix popup

--- script.js
+++ script.js
@@ -19,6 +19,22 @@ const exits = modal.querySelectorAll(".modal-exit");
     });
 });
 
+
+if (window.location.hash.indexOf("loginForm") !== -1) {
+        console.log(window.location.hash);
+        modal.classList.add("open");
+    exits.forEach(function(exit) {
+        exit.addEventListener("click", function() {
+            modal.classList.remove("open");
+            history.pushState("", document.title, window.location.pathname + window.location.search);
+        });
+    });
+    window.onhashchange = function() {
+        modal.classList.remove("open");
+        history.pushState("", document.title, window.location.pathname + window.location.search);
+    }
+}
+
 document.getElementById('go-back').addEventListener('click', () => {
     history.back();
   });
@@ -63,7 +79,5 @@ hamburgerClose.addEventListener('click', () => {
   hamburgerIcon.classList.remove('displayNone');
   nav.classList.remove('open');
 })
-
-
 
 
